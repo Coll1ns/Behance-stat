08bf057457692ff197b1ad9786f92ca27a71d66b
Coll1ns <coll1ns.ins1de@gmail.com>
2021-03-25 10:48:03 +0300

    added user name input

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import json
 from bs4 import BeautifulSoup
 import re
 
-
-html = requests.get('https://www.behance.net/tanya_durs').text
+username = input('Enter user name: ')
+url = 'https://www.behance.net/' + username
+html = requests.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
 value = soup.find('div', class_='UserInfo-column-TMV').text
 value = value.replace(",", "")
